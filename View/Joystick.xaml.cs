08b93f84d99619794b0303cf18d8dec48e40104d
TalZig <50695642+TalZig@users.noreply.github.com>
2020-03-18 19:17:54 +0200

    try to make the joystick work

--- View/Joystick.xaml.cs
+++ View/Joystick.xaml.cs
@@ -18,21 +18,41 @@ namespace FlightSimulator.View
     /// <summary>
     /// Interaction logic for Joystick.xaml
     /// </summary>
-    public partial class Joystick : Page
+    public partial class Joystick : UserControl
     {
         public Joystick()
         {
             InitializeComponent();
         }
-        private Point MouseDownLocation;
+        private Point center;
         private void centerKnob_Completed(Object sender, EventArgs e) { }
 
-        private void KnobBase_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Knob_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            if( )
+            center.X = 0;
+            center.Y = 0;
+        }
 
+        private void Knob_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                center = e.GetPosition(this);
+            }
+        }
 
+        private void Knob_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                double x = e.GetPosition(this).X - center.X;
+                double y = e.GetPosition(this).Y - center.Y;
+                if (Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) < Base.Width / 2)
+                {
+                    knobPosition.X = x;
+                    knobPosition.Y = y;
+                }
+            }
         }
     }
-    
 }
