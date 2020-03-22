f1dc79f0e07f8b416a088dac595edd6fe4607121
TalZig <50695642+TalZig@users.noreply.github.com>
2020-03-22 23:55:57 +0200

    added getters to values in the model

--- ViewModels/ViewModel.cs
+++ ViewModels/ViewModel.cs
@@ -83,6 +83,62 @@ namespace FlightSimulator.ViewModels
                 }
             }
         }
+        public double VMHeadingDeg
+        {
+            get
+            {
+                return model.HeadingDeg;
+            }
+        }
+        public double VMVerticalSpeed
+        {
+            get
+            {
+                return model.VerticalSpeed;
+            }
+        }
+        public double VMGroundSpeedKt
+        {
+            get
+            {
+                return model.GroundSpeedKt;
+            }
+        }
+        public double VMIndicatedSpeedKt
+        {
+            get
+            {
+                return model.IndicatedSpeedKt;
+            }
+        }
+        public double VMAltitudeFt
+        {
+            get
+            {
+                return model.AltitudeFt;
+            }
+        }
+        public double VMRollDeg
+        {
+            get
+            {
+                return model.RollDeg;
+            }
+        }
+        public double VMPitchDeg
+        {
+            get
+            {
+                return model.PitchDeg;
+            }
+        }
+        public double VMIndicatedValueFt
+        {
+            get
+            {
+                return model.IndicatedAlitudeFt;
+            }
+        }
 
     }
 }
