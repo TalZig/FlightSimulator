70a68d5a2509c86e0c21cf28f08460f45cc7ced0
Yoel Sherwin <yoel.sherwin@gmail.co.il>
2020-03-24 12:19:01 +0200

    a

--- View/Joystick.xaml.cs
+++ View/Joystick.xaml.cs
@@ -53,18 +53,18 @@ namespace FlightSimulator.View
             {
                 double x = (e.GetPosition(this).X - center.X);
                 double y = (e.GetPosition(this).Y - center.Y);
-                if (Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) +( KnobBase.Width / 2) < BlackCircle.Width / 2)
+                if (Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) +( KnobBase.Width / 2) <= BlackCircle.Width / 2)
                 {
                     knobPosition.X = x;
                     knobPosition.Y = y;
                     //update on view model (normalized)
                     if(x<0)
                         RudderValue = (x-(KnobBase.Width/2)) / (BlackCircle.Width / 2);
-                    else
+                    else if (x!=0)
                         RudderValue = (x + (KnobBase.Width / 2)) / (BlackCircle.Width / 2);
                     if(y<0)
                         ElevatorValue = (y - (KnobBase.Height / 2)) / (BlackCircle.Height / 2);
-                    else
+                    else if (y!=0)
                         ElevatorValue = (y + (KnobBase.Height / 2)) / (BlackCircle.Height / 2);
                 }
             }
